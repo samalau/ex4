d3cc115deb5a7ff95ea883d4ed1b485a8ada2772
Sam Newmark <smanthanewmark@live.biu.ac.il>
2024-12-12 14:52:38 +0200

    ex4

--- ex4.c
+++ ex4.c
@@ -1,7 +1,7 @@
 /******************
-Name:
-ID:
-Assignment:
+Name: Samantha Newmark
+ID: 346587629
+Assignment: 4
 *******************/
 #include <stdio.h>
 #include <string.h>
@@ -12,9 +12,10 @@ void task3_parenthesis_validator();
 void task4_queens_battle();
 void task5_crossword_generator();
 
+int task = -1;
+
 int main()
-{
-    int task = -1;
+{   
     do
     {
         printf("Choose an option:\n"
@@ -25,12 +26,11 @@ int main()
                "5. Crossword Generator\n"
                "6. Exit\n");
 
-        if (scanf("%d", &task))
+        if (scanf(" %d", &task))
         {
             switch (task)
             {
             case 6:
-                printf("Goodbye!\n");
                 break;
             case 1:
                 task1_robot_paths();
@@ -54,20 +54,103 @@ int main()
         }
         else
         {
-            scanf("%*s");
+            if (scanf(" %*[^\n] %*c") == EOF) task = 6 ;
+            // scanf("%*s");
         }
 
     } while (task != 6);
+    printf("Goodbye!\n");
+}
+
+int getDistinctPaths(int x, int y) {
+    if (x == 0 || y == 0) {
+        return 1 ;
+    }
+    return getDistinctPaths(x - 1, y) + getDistinctPaths(x, y - 1) ;
 }
 
 void task1_robot_paths()
 {
-    // Todo
+    int x, y;
+    int totalDistinctPathsHome = 0 ;
+    int validCoordinates = 0 ;
+
+    while (validCoordinates != 2) {
+
+        printf("Please enter the coordinates of the robot (column, row):\n") ;
+
+        validCoordinates = scanf(" %d %d%*[^\n] %*c", &x, &y) ;
+
+        if (validCoordinates == EOF) {
+            task = 6 ;
+            break ;
+        }
+
+        if (validCoordinates != 2) {
+            scanf(" %*[^\n] %*c");
+            continue ;
+        }
+
+        if (x < 0 || y < 0) {
+            totalDistinctPathsHome = 0 ;
+        } else {
+            totalDistinctPathsHome = getDistinctPaths(x, y) ;
+        }
+    }
+
+    printf("The total number of paths the robot can take to reach home is: %d\n", totalDistinctPathsHome) ;
 }
+
+
+void initPyramid(float *pyramidData[5]) {
+    for (int i = 0 ; i < 5 ; i++) {
+        for (int j = i ; j < i + 1 ; j++) {
+            pyramidData[i][j] = -1 ;
+		}
+	}
+}
+
+
+int getWeight() {
+    float level0[1], level1[2], level2[3], level3[4], level4[5] ;
+    float *pyramidData[5] = {level0, level1, level2, level3, level4} ;
+    initPyramid(pyramidData) ;
+
+    int input = 0 ;
+    int nextWeight = -1 ;
+    int fullPyramid = 0 ;
+    while (!fullPyramid) {
+        printf("Please enter the weights of the cheerleaders:\n") ;
+
+        for (int i = 0; i < 5 ; i++) {
+            for (int j = i ; j < i + 1 ; j++)
+            {
+                input = scanf(" %.2f", &nextWeight) ;
+
+                if (input == EOF) return NULL ;
+
+                if (input != 1 || nextWeight < 0)
+                {
+                    scanf(" %*[^\n]") ;
+                    scanf(" %*c");
+                    prinf("Negative weights are not supported.\n") ;
+                    break ;
+                }
+                pyramidData[i][j] = nextWeight ;
+            }
+            if (input != 1 || nextWeight < 0) break;
+            
+        }
+    }
+}
+
 
 void task2_human_pyramid()
 {
-    // Todo
+     
+    getWeight() ;
+
+
 }
 
 void task3_parenthesis_validator()
